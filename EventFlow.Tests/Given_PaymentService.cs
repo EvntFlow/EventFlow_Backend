64045984fc23069b58733fad95593ae795182671
Trung <57174311+trungnt2910@users.noreply.github.com>
2025-04-22 21:05:02 +1000

    test: Improve coverage for `PaymentService`

--- EventFlow.Tests/Given_PaymentService.cs
+++ EventFlow.Tests/Given_PaymentService.cs
@@ -39,4 +39,104 @@ public class Given_PaymentService : BaseTest
             Assert.That(payments.Single().DisplayName, Is.EqualTo(cardDisplayName));
         });
     }
+
+    [Test]
+    public async Task When_CheckValidPaymentMethod()
+    {
+        using var dbContext = new ApplicationDbContext(_dbOptions);
+
+        var account1 = (await dbContext.Users.AddAsync(new Account{})).Entity;
+        var account1Id = Guid.Parse(account1.Id);
+        var account2 = (await dbContext.Users.AddAsync(new Account{})).Entity;
+        var account2Id = Guid.Parse(account2.Id);
+
+        var payment1 = (await dbContext.PaymentMethods.AddAsync(new()
+        {
+            Account = account1,
+            Type = "PaymentMethod",
+            DisplayName = "Generic Payment"
+        })).Entity;
+        var payment2 = (await dbContext.PaymentMethods.AddAsync(new Data.Db.CardPaymentMethod()
+        {
+            Account = account2,
+            Type = nameof(Data.Db.CardPaymentMethod),
+            DisplayName = "Mr. President's Card",
+            Number = "4242 4242 4242 4242",
+            Expiry = "04/75",
+            Name = "NGUYEN VAN THIEU",
+            Cvv = "304"
+        })).Entity;
+
+        await dbContext.SaveChangesAsync();
+
+        var paymentService = new PaymentService(_dbOptions);
+
+        foreach (var accountId in new Guid[] { account1Id, account2Id })
+        {
+            var otherAccountId = accountId == account1Id ? account2Id : account1Id;
+
+            Assert.That(
+                await paymentService.GetPaymentMethods(accountId).CountAsync(), Is.EqualTo(1)
+            );
+
+            foreach (var paymentMethod in
+                await paymentService.GetPaymentMethods(accountId).ToListAsync())
+            {
+                await Assert.MultipleAsync(async () =>
+                {
+                    Assert.That(
+                        await paymentService.IsValidPaymentMethod(paymentMethod.Id, accountId)
+                    );
+                    Assert.That(
+                        await paymentService.IsValidPaymentMethod(paymentMethod.Id, otherAccountId),
+                        Is.False
+                    );
+                    Assert.That(
+                        await paymentService.IsValidPaymentMethod(Guid.NewGuid(), accountId),
+                        Is.False
+                    );
+                    Assert.That(
+                        await paymentService.IsValidPaymentMethod(Guid.Empty, accountId),
+                        Is.False
+                    );
+                });
+            }
+        }
+    }
+
+    [Test]
+    public async Task When_PerformTransaction()
+    {
+        using var dbContext = new ApplicationDbContext(_dbOptions);
+
+        var account1 = (await dbContext.Users.AddAsync(new Account{})).Entity;
+        var account1Id = Guid.Parse(account1.Id);
+        var account2 = (await dbContext.Users.AddAsync(new Account{})).Entity;
+        var account2Id = Guid.Parse(account2.Id);
+
+        var payment1 = (await dbContext.PaymentMethods.AddAsync(new()
+        {
+            Account = account1,
+            Type = "PaymentMethod",
+            DisplayName = "Generic Payment"
+        })).Entity;
+        var payment2 = (await dbContext.PaymentMethods.AddAsync(new Data.Db.CardPaymentMethod()
+        {
+            Account = account2,
+            Type = nameof(Data.Db.CardPaymentMethod),
+            DisplayName = "Mr. President's Card",
+            Number = "4242 4242 4242 4242",
+            Expiry = "04/75",
+            Name = "NGUYEN VAN THIEU",
+            Cvv = "304"
+        })).Entity;
+
+        await dbContext.SaveChangesAsync();
+
+        var paymentService = new PaymentService(_dbOptions);
+        await paymentService.PerformTransaction(payment1.Id, payment2.Id, 42.0m);
+        await paymentService.PerformTransaction(payment2.Id, payment1.Id, 42.0m);
+
+        Assert.Pass();
+    }
 }
